d1206717540df7947daa3f8fe86a93af57e6b69e
pacolahd <mbuntangwe@gmail.com>
2024-08-07 12:29:14 +0000

    custom_bottom_nav_bar updated

--- lib/src/dashboard/views/custom_bottom_nav_bar.dart
+++ lib/src/dashboard/views/custom_bottom_nav_bar.dart
@@ -1,9 +1,10 @@
-import 'package:afrosine/core/common/app/providers/user_provider.dart';
+import 'dart:async';
+
+import 'package:afrosine/core/common/widgets/custom_list_tile_2.dart';
 import 'package:afrosine/core/extensions/context_extension.dart';
 import 'package:afrosine/core/services/injection_container.dart';
-import 'package:afrosine/src/auth/data/models/user_model.dart';
 import 'package:afrosine/src/auth/presentation/bloc/auth_bloc.dart';
-import 'package:afrosine/src/dashboard/utils/dashboard_utils.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -54,8 +55,41 @@ class _CustomBottomNavBarState extends State<CustomBottomNavBar> {
       providers: [
         BlocProvider(create: (_) => sl<AuthBloc>()),
       ],
-      child: const Center(
-        child: Text('Profile'),
+      child: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Center(
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 100,
+                ),
+                const Text('Profile'),
+                Card(
+                  margin: const EdgeInsets.all(20),
+                  child: Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: CustomListTile2(
+                      leadingIcon: Icons.logout,
+                      title: 'Logout',
+                      value: true,
+                      onPressed: () async {
+                        final navigator = Navigator.of(context);
+                        await FirebaseAuth.instance.signOut();
+                        unawaited(
+                          navigator.pushNamedAndRemoveUntil(
+                            '/',
+                            (route) => false,
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     ),
   ];
@@ -82,79 +116,75 @@ class _CustomBottomNavBarState extends State<CustomBottomNavBar> {
     final unSelectedLabelStyle =
         Theme.of(context).bottomNavigationBarTheme.unselectedLabelStyle;
 
-    return StreamBuilder<LocalUserModel>(
-      stream: DashboardUtils.userDataStream,
-      builder: (_, snapshot) {
-        if (snapshot.hasData && snapshot.data is LocalUserModel) {
-          context.read<UserProvider>().setUser(snapshot.data!);
-
-          debugPrint('Userbbbbbbbbbbbb: ${snapshot.data!.toJson()}');
-          debugPrint('User: ${context.read<UserProvider>().user}');
-        }
-        return Scaffold(
-          body: IndexedStack(
-            index: _selectedBottomAppBarIndex,
-            children: _appSections,
+    // return StreamBuilder<LocalUserModel>(
+    //   stream: DashboardUtils.userDataStream,
+    //   builder: (_, snapshot) {
+    //     if (snapshot.hasData && snapshot.data is LocalUserModel) {
+    //       context.read<UserProvider>().setUser(snapshot.data!);
+    //
+    //       debugPrint('Userbbbbbbbbbbbb: ${snapshot.data!.toJson()}');
+    //       debugPrint('User: ${context.read<UserProvider>().user}');
+    //     }
+    return Scaffold(
+      body: IndexedStack(
+        index: _selectedBottomAppBarIndex,
+        children: _appSections,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        showUnselectedLabels: true,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(
+              _selectedBottomAppBarIndex == 0
+                  ? IconlyBold.home
+                  : IconlyLight.home,
+              color:
+                  _selectedBottomAppBarIndex == 0 ? Colors.orange : Colors.grey,
+            ),
+            label: 'Home',
+            backgroundColor: Colors.white,
           ),
-          bottomNavigationBar: BottomNavigationBar(
-            showUnselectedLabels: true,
-            items: <BottomNavigationBarItem>[
-              BottomNavigationBarItem(
-                icon: Icon(
-                  _selectedBottomAppBarIndex == 0
-                      ? IconlyBold.home
-                      : IconlyLight.home,
-                  color: _selectedBottomAppBarIndex == 0
-                      ? Colors.orange
-                      : Colors.grey,
-                ),
-                label: 'Home',
-                backgroundColor: Colors.white,
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(
-                  _selectedBottomAppBarIndex == 1
-                      ? IconlyBold.document
-                      : IconlyLight.document,
-                  color: _selectedBottomAppBarIndex == 1
-                      ? Colors.orange
-                      : Colors.grey,
-                ),
-                label: 'Materials',
-                backgroundColor: Colors.white,
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(
-                  _selectedBottomAppBarIndex == 2
-                      ? Icons.fastfood
-                      : Icons.fastfood_outlined,
-                  color: _selectedBottomAppBarIndex == 2
-                      ? Colors.orange
-                      : Colors.grey,
-                ),
-                label: 'Chop',
-                backgroundColor: Colors.white,
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(
-                  _selectedBottomAppBarIndex == 3
-                      ? IconlyBold.profile
-                      : IconlyLight.profile,
-                  color: _selectedBottomAppBarIndex == 3
-                      ? Colors.orange
-                      : Colors.grey,
-                ),
-                label: 'User',
-                backgroundColor: Colors.white,
-              ),
-            ],
-            currentIndex: _selectedBottomAppBarIndex,
-            selectedLabelStyle: selectedLabelStyle,
-            unselectedLabelStyle: unSelectedLabelStyle,
-            onTap: _onItemTapped,
+          BottomNavigationBarItem(
+            icon: Icon(
+              _selectedBottomAppBarIndex == 1
+                  ? IconlyBold.document
+                  : IconlyLight.document,
+              color:
+                  _selectedBottomAppBarIndex == 1 ? Colors.orange : Colors.grey,
+            ),
+            label: 'Materials',
+            backgroundColor: Colors.white,
           ),
-        );
-      },
+          BottomNavigationBarItem(
+            icon: Icon(
+              _selectedBottomAppBarIndex == 2
+                  ? Icons.fastfood
+                  : Icons.fastfood_outlined,
+              color:
+                  _selectedBottomAppBarIndex == 2 ? Colors.orange : Colors.grey,
+            ),
+            label: 'Chop',
+            backgroundColor: Colors.white,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              _selectedBottomAppBarIndex == 3
+                  ? IconlyBold.profile
+                  : IconlyLight.profile,
+              color:
+                  _selectedBottomAppBarIndex == 3 ? Colors.orange : Colors.grey,
+            ),
+            label: 'User',
+            backgroundColor: Colors.white,
+          ),
+        ],
+        currentIndex: _selectedBottomAppBarIndex,
+        selectedLabelStyle: selectedLabelStyle,
+        unselectedLabelStyle: unSelectedLabelStyle,
+        onTap: _onItemTapped,
+      ),
     );
+    //   },
+    // );
   }
 }
